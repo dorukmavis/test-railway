fdafaec86395fc93de8bd4c9573cb1d4d28c2e50
dorukmavis <mdorukmavis@gmail.com>
2023-03-26 21:39:32 +0300

    final-upload

--- main.py
+++ main.py
@@ -1,8 +1,46 @@
 from fastapi import FastAPI
-from utils import get_webdriver
+from utils import get_webdriver,checking_for_high_res,checking_for_hindi,checking_for_js_hovers
 from typing import Union
 
 app = FastAPI()
+
+LINKS = [
+    "https://graceful-sunburst-78f35d.netlify.app/www.classcentral.com/index.html",
+    "https://ammardab3an99.github.io/",
+    "https://heartfelt-lollipop-736861.netlify.app/",
+    "https://radiant-hummingbird-697a83.netlify.app/",
+    "http://trialserver.rf.gd/trial6/www.classcentral.com/index.html",
+    "https://class-central.vercel.app/www.classcentral.com/index.html",
+    "https://5dcookie.github.io/",
+    "https://www.census2011.co.in/city.php#:~:text=Here%20are%20the%20details%20of,%2D%203%2C124%2C458%2C%20Jaipur%20%2D%203%2C046%2C163",
+    "https://umair1814.github.io/",
+    "https://na.co",
+    "https://www.httrack.com/",
+    "https://www.classcentral.com/",
+    "https://sinistersup.github.io/classcentral-hindi/",
+    "https://omarkhalifa97.github.io/khalifaaaz.github.io/",
+    "https://df0a.github.io/",
+    "http://miguel-hindi-classcentral.s3-website-us-east-1.amazonaws.com/",
+    "https://classcentral-scrape-hindi.vercel.app/",
+    "https://testclass01.000webhostapp.com/www.classcentral.com/index.html"
+    "https://mike-brown5.github.io/AllStars-Scrapped-Site/",
+    "https://ubadeakkad.github.io/",
+    "https://www.dqlab.id/",
+    "https://drive.google.com/drive/folders/1eLU2AQ80bn89gYNC2HEIbrKswVzdj0qC?usp=sharing",
+    "https://www.mediafire.com/folder/ktcts50c9o1up/Class_Central_-Website_translation",
+    "https://www.facebook.com/",
+    "http://daniel1uno.byethost31.com/",
+    "http://mihir.testing-phase.com/",
+    "http://grsmuche.vh109.hosterby.com/",
+    "https://a58-mohiuddin.github.io/developer_trial_task/web_translate.html"
+    "https://datta07.github.io/classcentralclone/",
+    "https://venerable-froyo-7f3b1e.netlify.app/university/cornell.html"
+    "https://muhammederenaslan.github.io/codingallstars/",
+    "https://exosmotic-bay.000webhostapp.com/",
+    "https://interviewdeneme123.000webhostapp.com/",
+    "http://classcent.infinityfreeapp.com/",
+    "https://codingallstarttest0000.on.drv.tw/www.teeest.blog/"
+]
 
 
 
@@ -16,9 +54,31 @@ async def main_page() ->dict[str,str]:
         '--disable-dev-shm-usage'
     ]
 )
-    driver.get('https://google.com')
+    driver.get('"https://google.com')
     driver.close()
     return {
         "message":"Hello from fastapi"
     }
+
+
+
+@app.get('/test_links')
+async def testing_links() ->dict[str,dict]:
+    results = []
+    for link in LINKS:
+        hover_test = checking_for_js_hovers(link)
+        high_res_test = checking_for_high_res(link)
+        non_hindi_list = checking_for_hindi(link)
+        results.append({
+            'url':link,
+            'hover_test':hover_test,
+            'high_res_test':high_res_test,
+            'non_hindi_statements':non_hindi_list
+        })
+    
+    return {
+        'results':results
+    }
+
+
 
--- utils.py
+++ utils.py
@@ -1,6 +1,33 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from typing import Union
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import codecs,string
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
+import PIL.Image as Img
+from selenium.webdriver.common.action_chains import ActionChains
+from time import sleep
+import re
+
+
+PATH = os.getcwd()
+
+
+
+
+
+
+
+def check_hindi(character):
+    maxchar = max(character)
+    if u'\u0900' <= maxchar <= u'\u097f':
+        return True
+    else:
+      return False
+
 
 
 def get_webdriver(
@@ -11,6 +38,7 @@ def get_webdriver(
         chrome_opt = webdriver.ChromeOptions()
         for option in options:
             chrome_opt.add_argument(option)
+        chrome_opt.add_experimental_option('excludeSwitches', ['enable-logging'])
 
         chrome_driver = webdriver.Chrome(
             ChromeDriverManager().install(),
@@ -22,3 +50,82 @@ def get_webdriver(
     
     return chrome_driver
 
+
+
+
+def checking_for_hindi(link:str)->list:
+    driver=get_webdriver(options=["--headless","--no-sandbox",
+        '--disable-dev-shm-usage'])
+    try:
+        driver.get(link)
+        all_texts =WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body"))).text
+        all_texts = re.sub(r'[~^0-9]', '', all_texts)
+        print(all_texts)
+        # all_texts = driver.find_element(By.XPATH, "/html/body").text
+        not_hindi_words = [text for text in all_texts.split('\n') if not check_hindi(text)]
+        
+        driver.implicitly_wait(1)
+        return not_hindi_words
+    except:
+        return None
+
+
+    
+        
+
+def checking_for_high_res(link:str)->bool:
+    driver=get_webdriver(options=["--headless","--no-sandbox",
+        '--disable-dev-shm-usage'])
+    driver.get(link)
+    all_images = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.TAG_NAME,"img")))
+    all_images = [image for image in all_images if not ((image.size["width"] ==0) or (image.size['height']==0))]
+    presented_resolutions = [image.size for image in all_images]
+    actual_resolutions = [{"height":image.get_attribute('naturalHeight'),"width":image.get_attribute('naturalWidth')} for image in all_images]
+    checker = []
+    for presented,actual in zip(presented_resolutions,actual_resolutions):
+        w_d = float(int(actual['width']) - int(presented['width']) )
+        h_d = float(int(actual['height']) - int(presented['height']))
+        if w_d>0 and h_d>0:
+            checker.append(True)
+        elif (int(presented['width'])<int(actual['width']*6)) and (int(presented['height'])<int(actual['height'])*6):
+            checker.append(True)
+        else:
+            checker.append(False)
+
+    return all(checker)
+    # list_of_image_urls = [image.get_attribute('src') for image in all_images]
+
+
+
+
+
+def checking_for_js_hovers(link:str)->bool:
+    driver=get_webdriver(options=["--headless","--no-sandbox",
+    '--disable-dev-shm-usage'])
+    driver.get(link)
+    try:
+        elements_to_hover= WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR,"button.hidden.weight-semi.large-up-block")))
+        hover = ActionChains(driver).move_to_element(elements_to_hover)
+        hover.perform()
+        # main-nav-dropdown js-main-nav-dropdown
+        hovered_nav = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR,'nav.main-nav-dropdown.js-main-nav-dropdown.is-open')))
+        if hovered_nav:
+            # new_elements = driver.find_elements_by_css_selector('li.main-nav-dropdown__item')
+            # for new_element in new_elements:
+            #     hover.move_to_element(new_element).perform()
+            driver.close()
+            return True
+        else:
+            driver.close()
+            return False
+    except:
+        driver.close()
+        return False
+
+
+
+
+
+
+if __name__ == "__main__":
+    checking_for_js_hovers()
