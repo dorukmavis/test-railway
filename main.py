4935bb8393ad4ece1ec1befc8932d1f9625af336
dorukmavis <mdorukmavis@gmail.com>
2023-03-26 16:49:59 +0300

    changes

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ from utils import get_webdriver
 from typing import Union
 
 app = FastAPI()
-driver = get_webdriver(
-    [
-        "--headless"
-    ]
-)
+
 
 
 
 @app.get('/')
 async def main_page() ->dict[str,str]:
+    driver = get_webdriver(
+    [
+        "--headless"
+    ]
+)
+    driver.get('https://google.com')
     return {
         "message":"Hello from fastapi"
     }
